bab86ca80e8763ccf628c0012b330d4be483f7f8
Furkan Gözükara <furkangozukara@gmail.com>
2022-03-24 15:35:52 +0300

    lecture 3 part 2

---
+++ lecture 3/lecture 3/ViewComponents/PriorityListViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lecture_3
+{
+    public class PriorityListViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(
+        int maxPriority, bool isDone)
+        {
+            var items = await GetItemsAsync(maxPriority, isDone);
+            return View("PriorityList", items);
+        }
+        private async Task<List<int>> GetItemsAsync(int maxPriority, bool isDone)
+        {
+            return await Task.FromResult(
+
+                 new List<int> { DateTime.Now.Minute, DateTime.Now.Millisecond }
+
+                );
+
+        }
+    }
+}
