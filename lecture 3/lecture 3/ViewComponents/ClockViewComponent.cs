cda3ac7f011694e7e1eb2965d40671966cbf8618
Furkan Gözükara <furkangozukara@gmail.com>
2022-03-24 16:22:28 +0300

    lecture 3 part 4

---
+++ lecture 3/lecture 3/ViewComponents/ClockViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lecture_3.ViewComponents
+{
+    public class ClockViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("Clock");
+        }
+    }
+}
