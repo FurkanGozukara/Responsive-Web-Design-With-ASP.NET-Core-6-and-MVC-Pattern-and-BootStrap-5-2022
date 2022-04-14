be9d63fc45a74b7697335bea750a4799964b5711
Furkan Gözükara <furkangozukara@gmail.com>
2022-04-14 15:30:24 +0300

    lecture 5 fixed

--- lecture 5/lecture 5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace lecture_5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
